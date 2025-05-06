7b6d7557cd415085d858a3e82a88cfe8aed6b2e3
gela <175582756+GelaLela@users.noreply.github.com>
2025-05-06 12:06:56 +0800

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className="footer">
       <span className="osk">@_Teryu</span>
-      <span className="copyright">| ©2025-2026 | HOLOLIVE | COVER CORP</span>
+      <span className="copyright">| ©2025-2026 | HOLOLIVE | COVER CORP | CLICK MY LOGO TO SEE MORE OF MY WORKS</span>
     </div>
   );
 } 
